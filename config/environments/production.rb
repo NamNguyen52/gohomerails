a675b3b0902d1e1cddb0e86170c73f292d6b8eb3
NamNguyen52 <nam.nguyen52@gmail.com>
2015-03-12 12:38:54 -0700

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,7 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
+  config.serve_static_assets = true
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
